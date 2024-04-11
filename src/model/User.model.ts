a64e616ebd6a447879aabc03094e060528e7d59e
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-04-11 10:32:36 +0530

    user model

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -11,8 +11,9 @@ export interface User extends mongoose.Document {
   password: string;
   verifyCode: string;
   verifyCodeExpiry: Date;
+  isVerified: boolean;
   isAcceptingMessage: boolean;
-  message: Message[];
+  messages: Message[];
 }
 
 const MessageSchema: mongoose.Schema<Message> = new mongoose.Schema({
@@ -32,5 +33,40 @@ const UserSchema: mongoose.Schema<User> = new mongoose.Schema({
     type: String,
     required: [true, "Username is required"],
     trim: true,
+    unique: true,
   },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9. -]+\\. [a-zA-Z]{2,}$/,
+      "Please use a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
 });
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
