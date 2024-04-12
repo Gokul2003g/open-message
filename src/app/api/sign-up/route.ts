b2b7364361ee3f8d632bdfa32ca71584202acd1e
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-04-12 21:41:52 +0530

    email-template

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,17 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, email, password } = await request.json();
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      { success: false, message: "Error registering user" },
+      { status: 500 },
+    );
+  }
+}
